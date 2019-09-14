4d43edb76637d0d0982c2d3800c1a45678b13bdf
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2019-09-14 20:30:04 +0200

    just test codes

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+$request = $_SERVER['REQUEST_URI'];
+
+switch ($request) {
+    case '/' :
+        require __DIR__ . '/views/index.php';
+        break;
+    case '' :
+        require __DIR__ . '/views/index.php';
+        break;
+    case '/about' :
+        require __DIR__ . '/views/about.php';
+        break;
+    default:
+        require __DIR__ . '/views/404.php';
+        break;
+}
+?>
