017944254b4134576c44ebc47f2df8e533255e0b
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2019-09-15 20:45:03 +0200

    Create router.php

---
+++ web/router.php
@@ -0,0 +1,16 @@
+<?php
+class Router{
+	private $request;
+	public function __construct($request){
+		$this->request = $request;
+	}
+	public function get($route, $file){
+		$uri = trim( $this->request, "/" );
+		$uri = explode("/", $uri);
+		if($uri[0] == trim($route, "/")){
+			array_shift($uri);
+			$args = $uri;
+			require $file . '.php';
+		}
+	}
+}
