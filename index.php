def3930e3ded048e7e558def0690749efe620823
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2019-09-15 16:50:36 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,16 +4,16 @@ $request = $_SERVER['REQUEST_URI'];
 
 switch ($request) {
     case '/' :
-        require __DIR__ . '/views/index.php';
+        require __DIR__ . '/public/views/index.php';
         break;
     case '' :
-        require __DIR__ . '/views/index.php';
+        require __DIR__ . '/public/views/index.php';
         break;
     case '/about' :
-        require __DIR__ . '/views/about.php';
+        require __DIR__ . '/public/views/about.php';
         break;
     default:
-        require __DIR__ . '/views/404.php';
+        require __DIR__ . '/public/views/404.php';
         break;
 }
 ?>
