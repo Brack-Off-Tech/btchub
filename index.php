28edb44cd73a47436fdbf2fc03b0a0d143b3ed11
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2019-09-15 20:13:29 +0200

    update

--- index.php
+++ index.php
@@ -12,6 +12,9 @@ switch ($request) {
     case '/about' :
         require __DIR__ . '/public/views/about.php';
         break;
+    case '/contact' :
+        require __DIR__ . '/public/views/contact.php';
+        break;
     default:
         require __DIR__ . '/public/views/404.php';
         break;
