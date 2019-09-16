a44db1a0918e0d0c81f4648a16bd04f8b5cbc13b
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2019-09-16 18:56:07 +0200

    update

--- admincp/index.php
+++ admincp/index.php
@@ -8,13 +8,7 @@ switch ($request) {
     case '' :
         require __DIR__ . '/public/views/index.php';
         break;
-    case '/dashboard' :
+    case 'admin/dashboard' :
         require __DIR__ . '/public/views/components/dashboard.php';
         break;
-    case '/contact' :
-        require __DIR__ . '/public/views/components/contact.php';
-        break;
-    default:
-        require __DIR__ . '/public/views/components/404.php';
-        break;
-}
+    
