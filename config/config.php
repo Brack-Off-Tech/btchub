e7f30ad5a560c9014f08bcfdada9d1ff4d99cb65
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2019-12-31 07:56:22 -0800

    Update config.php

--- config/config.php
+++ config/config.php
@@ -11,4 +11,29 @@ define('DB_PASSWORD', '');
 
 // FILE PATHS
 define("PATH_CONFIG", PATH_ROOT . "config" . DIRECTORY_SEPARATOR);
+
+
+/**
+ * Used to store website configuration information.
+ *
+ * @var string or null
+ */
+function config($key = '')
+{
+    $config = [
+        'name' => 'Simple PHP Website',
+        'site_url' => '',
+        'pretty_uri' => true,
+        'nav_menu' => [
+            '' => 'Home',
+            'about-us' => 'About Us',
+            'products' => 'Products',
+            'contact' => 'Contact',
+        ],
+        'template_path' => 'template',
+        'content_path' => 'content',
+        'version' => 'v3.0',
+    ];
+    return isset($config[$key]) ? $config[$key] : null;
+
 ?>
